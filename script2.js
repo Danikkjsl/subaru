9c10593ec27fa6d4d101acc49edc068888f4e383
Danikkjsl <142660535+Danikkjsl@users.noreply.github.com>
2024-10-27 19:46:29 +0200

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,22 @@
+const burgerMenu = document.querySelector('.burger-menu');
+const navList = document.querySelector('.list');
+const header = document.querySelector('header');
+let lastScrollTop = 0;
+navList.classList.toggle('active');
+
+
+burgerMenu.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
+
+window.addEventListener('scroll', () => {
+    const currentScroll = window.scrollY;
+    if(currentScroll > lastScrollTop){
+        header.classList.add('hidden');
+        
+    }
+    else{
+        header.classList.remove('hidden')
+    }
+    lastScrollTop = currentScroll;
+})
